465f5f860d98f4e64af7ce591ac3183f4c528452
Sylvester Francis <sylvesterranjithfrancis@gmail.com>
2026-03-21 00:16:13 -0400

    chore: correct version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 5
-        versionName = "1.3.0"
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
